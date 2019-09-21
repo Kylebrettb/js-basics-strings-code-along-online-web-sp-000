723aa4cabbb41fcc5dcceedb15a1993d1c6cca59
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-21 20:58:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 
+const currentUser = 'User';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+ 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
